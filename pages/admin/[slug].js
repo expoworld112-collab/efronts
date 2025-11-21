425de2afc9223ce4aa7b8e4f8a0d18c87b91de47
expo <expoworld112@gmail.com>
2025-11-21 09:01:18 +0530

    slug

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,16 +1,33 @@
-// import Layout from '../../../components/Layout';
+// // import Layout from '../../../components/Layout';
+// import Admin from '../../components/auth/Admin';
+// import BlogUpdate from '../../components/crud/BlogUpdate';
+// // import Link from 'next/link';
+
+// const Blog = () => {
+//     return (
+
+//         <Admin>
+//             <BlogUpdate />
+//         </Admin>
+
+//     );
+// };
+
+// export default Blog;
+
+
+import dynamic from 'next/dynamic';
 import Admin from '../../components/auth/Admin';
-import BlogUpdate from '../../components/crud/BlogUpdate';
-// import Link from 'next/link';
+
+// Dynamically import BlogUpdate to avoid SSR issues
+const BlogUpdate = dynamic(() => import('../../components/crud/BlogUpdate'), { ssr: false });
 
 const Blog = () => {
-    return (
-
-        <Admin>
-            <BlogUpdate />
-        </Admin>
-
-    );
+  return (
+    <Admin>
+      <BlogUpdate />
+    </Admin>
+  );
 };
 
 export default Blog;
