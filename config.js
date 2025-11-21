aa87fafd2fe01c8aae99bad86acb170022e57c7e
expo <expoworld112@gmail.com>
2025-11-21 14:36:34 +0530

    config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-export const API = "https://ebacks-tau.vercel.app/api";
+export const API = "https://ebacks-woad.vercel.app/api";
 export const DOMAIN = "https://efronts-qwtfxsthi-expos-projects-8ebd0dc9.vercel.app/";
-export const BACKEND = "https://ebacks-tau.vercel.app/";
+export const BACKEND = "https://ebacks-woad.vercel.app/";
 
 
 // export const API = 'http://localhost:8000/api';
@@ -12,4 +12,4 @@ export const BACKEND = "https://ebacks-tau.vercel.app/";
 export const APP_NAME = "EXPO WORLD";
 export const DISQUS_SHORTNAME = "my-cms-7";
 export const APP_DESCRIPTION = "Expoworld is your ultimate travel companion, bringing you the best travel insights, destination guides, and expert tips to make your adventures unforgettable. Whether you're a solo traveler, a family vacationer, or an adventure seeker, Expoworld offers in-depth articles on breathtaking locations, cultural experiences, must-visit attractions, and hidden gems worldwide."
-export const MY_API = "https://ebacks-tau.vercel.app/";
+export const MY_API = "https://ebacks-woad.vercel.app/";
